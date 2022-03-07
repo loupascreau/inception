576db448b093905e699214fd052d73ff5eb93082
lpascrea <lpascrea@student.42.fr>
2022-03-07 07:23:16 -0500

    mise en place des relations entre containeurs, comment les lier par le reseau docker

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -2,12 +2,10 @@
 
 service mysql start
 
-echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
-echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root';" | mysql -u root --skip-password
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootpassword';" | mysql -u root --skip-password
-echo "CREATE USER 'lpascrea'@'localhost' IDENTIFIED BY 'lpascreapassword';" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON * . * TO 'lpascrea'@'localhost';" | mysql -u root --skip-password
+echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = '$MYSQL_USER';" | mysql -u root --skip-password
+echo "UPDATE mysql.user SET Password = PASSWORD('$MYSQL_PASSWORD') WHERE User = '$MYSQL_USER';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
 mysqld
--- srcs/requirements/wordpress/conf/wordpress/wp-config.php
+++ srcs/requirements/wordpress/conf/wordpress/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'MYSQL_DATABASE' );
 
 /** Database username */
-define( 'DB_USER', 'lpascrea' );
+define( 'DB_USER', 'MYSQL_USER' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'lpascreapassword' );
+define( 'DB_PASSWORD', 'MYSQL_PASSWORD' );
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1:3306' );
+define( 'DB_HOST', 'db:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
