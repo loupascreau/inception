efafd738cdae7e91901464e931bb7db2152c9831
lpascrea <lpascrea@student.42.fr>
2022-03-08 10:15:16 -0500

    toujours en train de chercher a relier mon docker mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,10 +2,12 @@
 
 service mysql start
 
-echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
-echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = '$MYSQL_USER';" | mysql -u root --skip-password
-echo "UPDATE mysql.user SET Password = PASSWORD('$MYSQL_PASSWORD') WHERE User = '$MYSQL_USER';" | mysql -u root --skip-password
+echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
+echo "CREATE USER 'wp'@'localhost' IDENTIFIED BY 'wppassword';" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON * . * TO 'wp'@'localhost';" | mysql -u root --skip-password
+# echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
+# echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root';" | mysql -u root --skip-password
+# echo "UPDATE mysql.user SET Password = PASSWORD('rootpassword') WHERE User = 'root';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
-mysqld
+mysqld -u root
