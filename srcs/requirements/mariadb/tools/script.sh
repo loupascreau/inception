6bb0021c17296fe1a9e8fd80e9fceea2539c2b82
lpascrea <lpascrea@student.42.fr>
2022-03-09 06:07:24 -0500

    toujours le meme probleme avec ma base de donnee

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,12 +2,12 @@
 
 service mysql start
 
-echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-echo "CREATE USER 'wp'@'localhost' IDENTIFIED BY 'wppassword';" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON * . * TO 'wp'@'localhost';" | mysql -u root --skip-password
-# echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
-# echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root';" | mysql -u root --skip-password
-# echo "UPDATE mysql.user SET Password = PASSWORD('rootpassword') WHERE User = 'root';" | mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+echo "CREATE DATABASE wordpress;" | mysql #-u root --skip-password
+echo "GRANT ALL ON wordpress.* TO 'wp'@'localhost' IDENTIFIED BY 'wppassword' WITH GRANT OPTION;" | mysql #-u root --skip-password
+# echo "GRANT ALL PRIVILEGES ON * . * TO 'wp'@'localhost';" | mysql #-u root --skip-password
+# echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql #-u root --skip-password
+# echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root';" | mysql #-u root --skip-password
+echo "UPDATE mysql.user SET Password = PASSWORD('rootpassword') WHERE User = 'root';" | mysql #-u root --skip-password
+echo "FLUSH PRIVILEGES;" | mysql #-u root --skip-password
 
-mysqld -u root
+exec /usr/sbin/mysqld
